f9598d65a3845fbcfc01d6fecd08b67b4066bd25
Morris <morrisho215215@gmail.com>
2023-10-14 03:49:00 -0400

    Set up Tensorflow

--- setting_up.sh
+++ setting_up.sh
@@ -4,12 +4,16 @@
 
 # Detect the operating system
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    pip install -r requirements.txt
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
     my_string="I dont want to do this yet lmao"
     echo $my_string
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    pip install -r requirements.txt
+
 elif [[ "$OSTYPE" == "win32" ]]; then
     pip install -r requirements.txt
+    pip install cython
+    pip install git+https://github.com/philferriere/cocoapi.git
 else
     echo "Unsupported OS"
 fi
